7ca7406d37500e079221b520785e421b7248d83b
Deanbarber <dib47620@uga.edu>
2021-08-07 21:28:32 -0400

    Done for this Build

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import Image from "next/image"
+import { motion } from "framer-motion"
 
 
 function MediumCard({img, title}) {
     return (
-        
-            <div className=" animate-wiggle   cursor-pointer hover:scale-105 transition transform duration-300 ease-out" >
+      <motion.button
+      whileHover={{ scale: 0.9}}
+      whileTap={{ scale: 1 }}> 
+        <div className=" animate-wiggle   cursor-pointer hover:scale-105 transition transform duration-300 ease-out" >
             <div className="relative h-80 w-80">
               <Image src= {img} layout="fill" className="rounded-xl"/>  
             </div>
             <h3 className="text-2xl ml-3">{title}</h3>
         </div>
+      </motion.button>
+            
             
         
         
